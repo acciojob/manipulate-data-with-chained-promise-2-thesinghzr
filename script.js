0b751d72834e99b212fbe4cf812195de5bd4e845
Suraj Singh <yousurajsingh@gmail.com>
2025-03-28 23:58:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+
+let outputDiv = document.querySelector("#output");
+
+function promise1(){
+	return new Promise((res, rej) => {
+		setTimeout(() => {
+			res([1, 2, 3, 4]);
+		},3000)
+	});
+};
+
+function promise2(arr) {
+	return new Promise((res, rej) => {
+		setTimeout(() => {
+			res(arr.filter((num) => num % 2 === 0));
+		},1000)
+	});
+};
+
+function promise3(arr) {
+	return new Promise((res, rej) => {
+		setTimeout(() => {
+			res(arr.map((num) => num * 2));
+		},2000)
+	});
+};
+
+promise1().then((res) => {
+	return promise2(res);
+}).then((res)=>{
+	outputDiv.textContent = res.join(",");
+	return promise3(res);
+}).then((res)=>{
+	outputDiv.textContent = res.join(",");
+});
+
+
